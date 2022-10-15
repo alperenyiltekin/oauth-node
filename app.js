794e524311100f08367f9be6e23731b6af5ecdea
Alperen Yıltekin <ayiltekin@protel.com.tr>
2022-10-15 18:29:07 +0300

    google strategy added

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const path  = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 // Load config env
 dotenv.config({
-    path: './config/config.env'
+    path: '.env'
 });
 
+require('./config/passport')(passport);
+
 // Connect mongodb
-connectDB().then().catch();
+connectDB()
+    .then(()=> console.log('Mongo Connection Successful'))
+    .catch(err => console.error('error:', err));
 
 const app = express();
 
@@ -24,7 +30,25 @@ app.engine('handlebars', hbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+// Session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
