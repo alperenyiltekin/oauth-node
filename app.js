de968d49854a16729c0b3574d4fd5706cc9f3244
Alperen Yıltekin <ayiltekin@protel.com.tr>
2022-10-16 01:12:01 +0300

    done...

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const express       = require('express');
-const mongoose      = require('mongoose');
-const dotenv        = require('dotenv');
-const morgan        = require('morgan');
-const path          = require('path');
-const hbs           = require('express-handlebars');
-const passport      = require('passport');
-const session       = require('express-session');
-const MongoStore    = require('connect-mongo')(session);
-const connectDB     = require('./config/db');
+const express           = require('express');
+const mongoose          = require('mongoose');
+const dotenv            = require('dotenv');
+const morgan            = require('morgan');
+const path              = require('path');
+const hbs               = require('express-handlebars');
+const passport          = require('passport');
+const methodOverride    = require('method-override');
+const session           = require('express-session');
+const MongoStore        = require('connect-mongo')(session);
+const connectDB         = require('./config/db');
+const {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select
+}                       = require('./helpers/hbs');
 
 dotenv.config({
     path: '.env'
@@ -15,7 +23,7 @@ dotenv.config({
 
 require('./config/passport')(passport);
 
-// Connect mongodb
+// Connect Mongo
 connectDB()
     .then(() => console.log('Mongo Connection Successful'))
     .catch(err => console.error('error:', err));
@@ -25,12 +33,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method Override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        let method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}))
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-app.engine('handlebars', hbs.engine());
+app.engine('handlebars', hbs.engine({
+    defaultLayout: 'main',
+    helpers: {
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+        select
+    }
+}));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
@@ -50,6 +76,11 @@ app.use(passport.session());
 
 // Static
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+})
 
 // Routes
 app.use('/', require('./routes/index'));
