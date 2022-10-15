fe80340e4bebb7d396a3c61be16fc20c1a85d9e0
Alperen Yıltekin <ayiltekin@protel.com.tr>
2022-10-16 01:13:15 +0300

    all project files completed

--- app.js
+++ app.js
@@ -88,7 +88,6 @@ app.use('/auth', require('./routes/auth'));
 app.use('/auth', require('./routes/stories'));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(
     PORT,
     () => console.log(`Server running on ${PORT} port`)
